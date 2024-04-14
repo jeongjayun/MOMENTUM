ecb42d78607622dfbb6942f95bbf9bcdab3ec46c
jeongjayun <1252564@gmail.com>
2024-04-15 02:40:16 +0900

    완료

---
+++ js/background.js
@@ -0,0 +1,20 @@
+const images = [
+  "01.JPG",
+  "02.jpg",
+  "03.jpg",
+  "04.jpg",
+  "05.jpg",
+  "06.jpeg",
+  "07.jpg",
+  "08.jpg",
+  "09.jpg",
+  "10.JPG",
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `/img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
---
+++ js/todo.js
@@ -0,0 +1,66 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteTodo(event) {
+  // console.log(event.target.parentElement.innerText);
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
+}
+
+function paintTodo(newTodo) {
+  //console.log("i will paint", newTodo); // newTodo의 값을 받는다
+  const li = document.createElement("li");
+  li.id = newTodo.id;
+
+  const span = document.createElement("span");
+  span.innerText = newTodo.text;
+
+  const button = document.createElement("button");
+
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
+
+  li.appendChild(span); //li에 span 을 넣는다
+  li.appendChild(button);
+
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value; //toDoInput의 내용을 newTodo에 복사!
+  toDoInput.value = "";
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintTodo(newTodoObj);
+  saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  console.log(parsedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintTodo);
+}
+
+function goodFilter() {
+  //반드시 true를 return 해야한다.
+}
